ff98d60b489ccc2b046f7df70e091cf713f7e869
pawt <pawt87@gmail.com>
2017-03-10 22:37:11 +0100

    Small git manipulation in gitignore file...

--- thanks/models.py
+++ thanks/models.py
@@ -6,10 +6,10 @@ class Employee(models.Model):
 	name = models.ForeignKey('auth.User')
 	team = models.CharField(max_length=200)
 	email = models.EmailField()
-	points_collected = models.IntegerField()
+	points_to_give = models.IntegerField()
 	points_received = models.IntegerField()
 
 	def __str__(self):
-		return self.name
+		return self.name.username
 
 	
