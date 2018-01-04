990bbb78280b27591ba28a506ed17f6c2c0a2003
pawt <pawt87@gmail.com>
2018-01-04 21:25:32 +0100

    Fixed problem with tabs/spaces

--- thanks/models.py
+++ thanks/models.py
@@ -4,24 +4,24 @@ from datetime import datetime
 from django.db import models
 
 class Employee(models.Model):
-	name = models.ForeignKey('auth.User')
-	points_to_give = models.IntegerField(default=100)
-	points_collected = models.IntegerField(default=0)
+    name = models.ForeignKey('auth.User')
+    points_to_give = models.IntegerField(default=100)
+    points_collected = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.name.username
+    def __str__(self):
+        return self.name.username
 
 
 class Transaction(models.Model):
-	giver = models.ForeignKey('thanks.Employee', related_name = "giver")
-	receiver = models.ForeignKey('thanks.Employee', related_name = "receiver")
-	description = models.TextField(default='')
-	points_given = models.IntegerField(default=100)
-	date = models.DateTimeField(default=datetime.now,blank=True)
+    giver = models.ForeignKey('thanks.Employee', related_name = "giver")
+    receiver = models.ForeignKey('thanks.Employee', related_name = "receiver")
+    description = models.TextField(default='')
+    points_given = models.IntegerField(default=100)
+    date = models.DateTimeField(default=datetime.now,blank=True)
 
-	def __str__(self):
-		return "Transaction #" + str(self.id) + str(self.giver) + " -> " + str(self.receiver)
+    def __str__(self):
+        return "Transaction #" + str(self.id) + str(self.giver) + " -> " + str(self.receiver)
 
-	def make(self):
+    def make(self):
         self.date = timezone.now()
         self.save()
